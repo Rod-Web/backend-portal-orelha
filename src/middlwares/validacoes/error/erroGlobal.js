8ad09c8eda380bc7c9fff594ff0f110f7b532f10
Rodrigo <rodrigovalentim2008@gmail.com>
2026-09-05 16:42:45 -0300

    Correção no middlware de erro, para não mandar pro usuário msg de erro 500

--- src/middlwares/validacoes/error/erroGlobal.js
+++ src/middlwares/validacoes/error/erroGlobal.js
@@ -1,5 +1,13 @@
 export function middlewaresError(err, req, res, next) {
     const status = err.status || 500
+    if(err.status == 500) {
+        console.err(err)
+        return res.status(status).json({
+            erro: "Erro interno no servidor. Informe o suporte técnico.",
+        });
+    };
+
+
     return res.status(status).json({
         erro: err.message || "Erro interno no servidor. Informe o suporte técnico."
     });
