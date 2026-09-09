e7f47b4d39b2c9efa106c309b829f56e540382c3
Rodrigo <rodrigovalentim2008@gmail.com>
2026-09-09 09:04:24 -0300

    correção no middlware de erro

--- src/middlwares/validacoes/error/erroGlobal.js
+++ src/middlwares/validacoes/error/erroGlobal.js
@@ -1,7 +1,7 @@
 export function middlewaresError(err, req, res, next) {
     const status = err.status || 500
-    if(err.status == 500) {
-        console.err(err)
+    if(status === 500) {
+        console.error(err)
         return res.status(status).json({
             erro: "Erro interno no servidor. Informe o suporte técnico.",
         });
