a27683fca57e4e7835709cbe0a70133d11548ed3
Rodrigo <rodrigovalentim2008@gmail.com>
2026-09-13 01:25:58 -0300

    teste 3 - adicionado secure true, e limite de tempo para os cookies, apagando os console de teste anterior

--- src/controller/login.js
+++ src/controller/login.js
@@ -4,19 +4,20 @@ export async function controllerLogin(req, res, next) {
     try {
         const {cpf, senha} = req.body;
         const {tokens, tipo_usuario} = await serviceLogar(cpf, senha);
-        console.log(tokens.accessToken + "oi" + tokens.refreshToken)
         res
         .cookie("accessToken", tokens.accessToken, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: "none",
-            path: '/rotasprivadas'
+            path: '/rotasprivadas',
+            maxAge: 15 * 60 * 1000
         })
         .cookie('refreshToken', tokens.refreshToken, {
             httpOnly: true, // JavasCript não pode ler
-            secure: false, // Só permite https
+            secure: true, // Só permite https
             sameSite: "none", // Permite cross-site, consumir a API em dominio diferente
-            path: '/auth/refresh'
+            path: '/auth/refresh',
+            maxAge: 7 * 24 * 60 * 60 * 1000
         })
         .status(200)
         .json({mensagem: "Login efetuado com sucesso.", tipo_usuario: tipo_usuario});
--- src/controller/refresh.js
+++ src/controller/refresh.js
@@ -5,12 +5,15 @@ export function controllerRefresh(req, res, next) {
     try {
         const refresh = req.cookies.refreshToken;
         const accessToken = serviceRefresh(refresh);
-        res.cookie("accessToken", accessToken, {
-          httpOnly: true,
-          secure: false,
-          sameSite: "none",
-          path: "/rotasprivadas",
-        }).sendStatus(204);
+        res
+          .cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            path: "/rotasprivadas",
+            maxAge: 15 * 60 * 1000,
+          })
+          .sendStatus(204);
 
     } catch (err) {
         next(err)
